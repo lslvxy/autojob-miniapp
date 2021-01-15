3043f0ac11103d9ed0fb3a049d1bc1ed6721daef
Laysan <lise@MacBook-Pro.local>
2021-01-15 15:29:41 +0800

    update

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -14,7 +14,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var type = options.type;
+    this.setData({
+      type: type
+    });
+    wx.setNavigationBarTitle({
+      title: app.globalData.modules[type] //页面标题为路由参数
+    })
   },
 
   /**
@@ -23,10 +29,11 @@ Page({
   onReady: function () {
     var _this = this;
     wx.request({
-      url: app.globalData.baseUrl + '/everphoto/log', //仅为示例，并非真实的接口地址
+      url: app.globalData.baseUrl + '/logs/list',
       method: 'POST',
       data: {
         userId: app.globalData.openId,
+        type: _this.data.type
       },
       header: {
         'content-type': 'application/json' // 默认值
